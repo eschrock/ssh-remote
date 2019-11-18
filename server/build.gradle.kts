c377f9093b4d9a92385e6a8ee8a28d4afb0bdfb3
Eric Schrock <eric.schrock@delphix.com>
2019-11-18 10:05:16 -0500

    update to stable remote-sdk (#6)

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -18,7 +18,7 @@ repositories {
 
 dependencies {
     compile(kotlin("stdlib"))
-    compile("io.titandata:remote-sdk:0.0.11")
+    compile("io.titandata:remote-sdk:0.1.0")
     compile("io.titandata:command-executor:0.0.10")
     compile("com.google.code.gson:gson:2.8.6")
     testImplementation("io.kotlintest:kotlintest-runner-junit5:3.4.2")
